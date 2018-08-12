fbfe2b4eca3b456c7eb89c55fff0b71bed5e1658
Matteo Zilio <matteo@mbp-di-matteo.homenet.telecomitalia.it>
2018-08-12 14:29:10 +0200

    Fixed first query

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 // =============================================================================
 
 // call the packages we need
-var express    = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
-var app        = express();
-var morgan     = require('morgan');
+var app = express();
+var morgan = require('morgan');
 
 // configure app
 app.use(morgan('dev')); // log requests to the console
@@ -14,21 +14,10 @@ app.use(morgan('dev')); // log requests to the console
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port       = process.env.PORT || 5000; // set our port
+var port = process.env.PORT || 5000; // set our port
 
 // DATABASE SETUP
 const { db } = require('./database');
-// const { Client } = require('pg');
-//
-// const pg_client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true,
-// });
-//
-// // Handle the connection event
-// pg_client.connect()
-//   .then(() => console.log('DB connected'))
-//   .catch(e => console.error('DB connection error', err.stack));
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -49,22 +38,11 @@ router.get('/', function(req, res) {
 });
 
 // TO DESCRIBE (accessed at GET http://localhost:5000/api/external_temp)
-router.get('/external_temp', function(req, res) {
-  res.json({ message: 'Shown the external temperature!' });
-  // pg_client.query('SELECT * FROM temp_ext ORDER BY datetime DESC LIMIT 1;', (err, res_query) => {
-  //   if (err) throw err;
-  //   for (let row of res_query.rows) {
-  //     res.send(JSON.stringify(row));
-  //   }
-  //   pg_client.end();
-  // });
-});
-
-router.get('/postgres', (req, res, next) => {
+router.get('/external_temp', (req, res, next) => {
   db
     .any('SELECT * FROM temp_ext ORDER BY datetime DESC LIMIT 1')
     .then(data => {
-      res.json(`${req.path} fetched ${JSON.stringify(data)} from the database`)
+      res.json(`${JSON.stringify(data)}`)
     })
     .catch(next)
 });
